c8dd3bde9371c58a83a9e6c9081042e8737a5c98
fatihcandev <fatihcan.social@gmail.com>
2020-12-13 15:14:36 +0300

    add animation to CheckIcon

--- src/components/icons/Check.tsx
+++ src/components/icons/Check.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SvgCheck() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
+    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9" className="check-icon">
       <path fill="none" stroke="#FFF" strokeWidth={2} d="M1 4.304L3.696 7l6-6" />
     </svg>
   );
