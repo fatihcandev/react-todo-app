45147523b197f7d45e5e5bf0036faf4fb4966843
fatihcandev <fatihcan.social@gmail.com>
2020-12-13 11:55:29 +0300

    Home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,67 @@
 import React from 'react';
 
-const Home = (): JSX.Element => {
-  return <div>Hello World</div>;
+import { Layout, TodoInput, TodoList, TodoListContainer, TodoListHeader } from '../components';
+import { ITodo } from '../types';
+
+const Home: React.FC = () => {
+  const [todos, setTodos] = React.useState<Set<ITodo>>(new Set());
+
+  const getTodos = React.useCallback(() => {
+    const todoList = localStorage.getItem('todos');
+    if (todoList !== null) {
+      setTodos(new Set(JSON.parse(todoList)));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    getTodos();
+  }, [getTodos]);
+
+  const handleAddToDo = (todo: ITodo) => {
+    setTodos(prevTodos => prevTodos.add(todo));
+    const newTodoList = JSON.stringify(Array.from(todos));
+    localStorage.setItem('todos', newTodoList);
+    getTodos();
+  };
+
+  const handleRemoveTodo = (id: string) => {
+    todos.forEach(todo => {
+      if (todo.id === id) {
+        todos.delete(todo);
+      }
+    });
+    const newTodoList = JSON.stringify(Array.from(todos));
+    localStorage.setItem('todos', newTodoList);
+    getTodos();
+  };
+
+  const handleCompleteTodo = (id: string, completed: boolean) => {
+    const todoArr = Array.from(todos);
+    const todo = todoArr.find(todo => todo.id === id);
+    if (todo) {
+      const index = todoArr.indexOf(todo);
+      todoArr.splice(index, 1, { ...todo, completed });
+      const newTodoList = JSON.stringify(todoArr);
+      localStorage.setItem('todos', newTodoList);
+      getTodos();
+    }
+  };
+
+  return (
+    <Layout todoAmount={todos.size}>
+      <TodoListContainer>
+        <TodoListHeader />
+        <TodoInput onAddTodo={handleAddToDo} />
+        {todos.size > 0 && (
+          <TodoList
+            todos={Array.from(todos)}
+            onCompleteClick={handleCompleteTodo}
+            onRemoveClick={handleRemoveTodo}
+          />
+        )}
+      </TodoListContainer>
+    </Layout>
+  );
 };
 
 export default Home;
