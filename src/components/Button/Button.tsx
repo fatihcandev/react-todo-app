35cd18aa4089e901d7ae9f8eb398aba1a79daac8
fatihcandev <fatihcan.social@gmail.com>
2020-12-15 00:25:24 +0300

    update Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,18 +8,9 @@ interface IButtonProps {
 }
 
 const Button: React.FC<IButtonProps> = ({ onClick, className, children }) => {
-  const [clicked, setClicked] = React.useState<boolean>(false);
-
-  const handleClick = () => {
-    setClicked(true);
-    onClick();
-    setTimeout(() => setClicked(false), 500);
-  };
-
   return (
-    <button onClick={handleClick} className={`${styles.container} ${className && className}`}>
+    <button {...{ onClick }} className={`${styles.container} ${className && className}`}>
       {children}
-      {clicked && <span className={styles.ripple} />}
     </button>
   );
 };
