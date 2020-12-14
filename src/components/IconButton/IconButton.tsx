ecb2c499dc4c092eb6822e9a987645b938f0ab80
fatihcandev <fatihcan.social@gmail.com>
2020-12-15 00:25:34 +0300

    update IconButton

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -3,6 +3,8 @@ import { Button } from '../Button';
 
 import { Icon } from '../Icon';
 
+import styles from './IconButton.module.scss';
+
 interface IIconButtonProps {
   icon: string;
   onClick: () => void;
@@ -11,7 +13,7 @@ interface IIconButtonProps {
 
 const IconButton: React.FC<IIconButtonProps> = ({ icon, onClick, ariaLabel }) => {
   return (
-    <Button {...{ onClick }} aria-label={ariaLabel}>
+    <Button {...{ onClick }} aria-label={ariaLabel} className={styles.container}>
       <Icon {...{ icon }} />
     </Button>
   );
